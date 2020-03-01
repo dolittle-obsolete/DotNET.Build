5a06e4bb3210d1f42b1fb16bc2e3d2d706aa27d6
Einar Ingebrigtsen <einar@dolittle.com>
2020-03-01 16:40:48 +0100

    Improved versioning support - pre-release++

--- release_from_local.sh
+++ release_from_local.sh
@@ -6,9 +6,25 @@ export PACKAGEDIR=$PWD/Artifacts/NuGet
 export DOLITTLERELEASE=true
 export VERSION=$(git tag --sort=-version:refname | head -1)
 
+MAJOR_VERSION=$(echo $VERSION | sed 's/v*\([0-9]*\).*$/\1/g')
+MINOR_VERSION=$(echo $VERSION | sed 's/v*[0-9]*.\([0-9]*\).*$/\1/g')
+PATCH_VERSION=$(echo $VERSION | sed 's/v*[0-9]*.[0-9]*.\([0-9]*\).*$/\1/g')
+PRE_RELEASE_TAG=$(echo $VERSION | sed 's/v*[0-9]*.[0.9]*.[0-9]-\([a-zA-Z]*\).*$/\1/g')
+BUILD_VERSION=$(echo $VERSION | sed 's/v*[0-9]*.[0.9]*.[0-9]-[a-zA-Z]*.\([0-9]*\)/\1/g')
+PACKAGE_VERSION=$MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION-$PRE_RELEASE_TAG.$BUILD_VERSION
+
+echo "Git Version : " $VERSION
+echo "Major Version : " $MAJOR_VERSION
+echo "Minor Version : " $MINOR_VERSION
+echo "Patch Version : " $PATCH_VERSION
+echo "Build Version : " $BUILD_VERSION
+echo "Package Version : " $PACKAGE_VERSION
+
+VERSION=
+
 rm -rf $PWD/Artifacts
 
-dotnet pack -p:PackageVersion=$VERSION -c release -o $PACKAGEDIR
+dotnet pack -p:PackageVersion=$PACKAGE_VERSION -c release -o $PACKAGEDIR
 
 for f in $PACKAGEDIR/*.symbols.nupkg
 do
